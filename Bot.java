f7a26b310e491419ac369cb026f824b1a64cbb03
Jake Sippy <mrjsippy@gmail.com>
2017-12-28 15:04:39 -0800

    Force commit

--- Bot.java
+++ Bot.java
@@ -1,4 +1,8 @@
+import java.util.*;
+
 public class Bot {
+    private static final int DEPTH = 3;
+
     private Board board;
     private Color color;
 
@@ -8,8 +12,38 @@ public class Bot {
     }
 
     public Move getMove() {
-        for (Move m : board.getLegalMoves())
-            return m;
-        return null;
+        ScoreTree(board);
+        for (int i = 0; i < DEPTH; i++) {
+            
+        }
+    }
+
+    public int getScore() {
+        int score = 0;
+        for (int i = 1; i <= board.getNumOfPlaces; i++) {
+            if (board.hasPieceAt(i)) {
+                int mult = 1;
+                if  (board.isKing(i))
+                    mult = 2;
+                
+                if (board.getColor(i) == this.color) {
+                    score += mult;
+                } else {
+                    score += mult;
+                }
+            }
+        }
+    }
+
+    class ScoreTree {
+        int depth;
+        int score;
+        List<ScoreTree> children;
+
+        ScoreTree(Board b, int depth) {
+            this.depth = depth;
+        }
+
+        
     }
 }
